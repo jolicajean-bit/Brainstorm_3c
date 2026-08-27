0d952e42a2e71674e4337ae9b86fd3bb363c1d1c
jolicajean-bit <jolica.jean@escola.pr.gov.br>
2026-08-27 09:30:50 -0300

    s

--- script.js
+++ script.js
@@ -58,7 +58,24 @@ const perguntas = [
   },
 ];
 
+let atual = 0;
+let perguntaAtual;
 
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+
+mostraPergunta();
+
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
 
 
  
